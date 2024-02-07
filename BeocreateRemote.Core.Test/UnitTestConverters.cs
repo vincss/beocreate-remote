d63fd4040b021f5b9604637a9f950c4012e40ed7
vwi <v.wiertz@evs.com>
2024-02-07 09:33:48 +0100

    ci & tests

---
+++ BeocreateRemote.Core.Test/UnitTestConverters.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeocreateRemote.Core.Test
+{
+    [TestClass]
+    public class UnitTestConverters
+    {
+
+        [TestMethod]
+        public void TestSigmaTcpConverter()
+        {
+            var dico = new Dictionary<double, byte[]>() {
+                { 0.1, new byte[] { 0, 25, 153, 153 } },
+                { 0.05, new byte[] { 0x00, 0x0C, 0xCC, 0xCC }},
+                { 0.01, new byte[] { 0x00, 0x02, 0x8F, 0x5C }},
+
+            };
+
+            foreach (var kvp in dico)
+            {
+                var actual = SigmaTcpController.DecimalVal(kvp.Value);
+                Assert.AreEqual(kvp.Key, actual);
+            }
+
+        }
+
+        [TestMethod]
+        public void SshControllerConvert()
+        {
+            var result = SshController.ConvertVolume("Volume: 0.0138 / 38% / -37db");
+            Assert.AreEqual(1, result);
+
+            result = SshController.ConvertVolume("Volume: 0.66 / 66% / -xxdb");
+            Assert.AreEqual(66, result);
+        }
+
+        [TestMethod]
+        public void SshControllerConvertBack()
+        {
+            var result = SshController.ConvertBackVolume(33);
+            Assert.AreEqual("0.33", result);
+        }
+    }
+
+}
--- BeocreateRemote.Core.Test/UnitTests.cs
+++ BeocreateRemote.Core.Test/UnitTests.cs
@@ -1,5 +1,6 @@
 namespace BeocreateRemote.Core.Test
 {
+    [Ignore]
     [TestClass]
     public class UnitTests
     {
@@ -22,41 +23,6 @@ namespace BeocreateRemote.Core.Test
             sigmaTcpController.Mute();
 
             sigmaTcpController.Unmute();
-        }
-
-        [TestMethod]
-        public void TestSigmaTcpConverter()
-        {
-            var dico = new Dictionary<double, byte[]>() { 
-                { 0.1, new byte[] { 0, 25, 153, 153 } },
-                { 0.05, new byte[] { 0x00, 0x0C, 0xCC, 0xCC }},
-                { 0.01, new byte[] { 0x00, 0x02, 0x8F, 0x5C }},
-
-            };
-
-            foreach (var kvp in dico)
-            {
-                var actual = SigmaTcpController.DecimalVal(kvp.Value);
-                Assert.AreEqual(kvp.Key, actual);
-            }
-
-        }
-
-        [TestMethod]
-        public void SshControllerConvert()
-        {
-            var result = SshController.ConvertVolume("Volume: 0.0138 / 38% / -37db");
-            Assert.AreEqual(1, result);
-
-            result = SshController.ConvertVolume("Volume: 0.66 / 66% / -xxdb");
-            Assert.AreEqual(66, result);
-        }
-
-        [TestMethod]
-        public void SshControllerConvertBack()
-        {
-            var result = SshController.ConvertBackVolume(33);
-            Assert.AreEqual("0.33", result);
         }
     }
 }
